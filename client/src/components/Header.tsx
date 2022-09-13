9922c0620f287d1cd29b5866dd8a4e283410e8d0
ysrckr <dyt.yasarcakir@gmail.com>
2022-09-13 22:31:46 +0300

    moved hero image to body fixed circle on the input field

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,20 +1,18 @@
 import { FC } from 'react'
 import type { header } from '../../types/propTypes'
-import { useState } from 'react'
 import Input from './Input'
 const Header: FC<header> = ({ theme, todo, changeHandler }) => {
 	const clickHandler = () => {}
 	return (
 		<header className={'flex-col ' + theme}>
-			<div className="hero">
 				<div className="flex">
 					<h1>TODO</h1>
 					<span onClick={clickHandler}>{theme}</span>
 				</div>
 				<div className="input-container">
+					<span className='input-circle'></span>
 					<Input todo={todo} changeHandler={changeHandler} />
 				</div>
-			</div>
 		</header>
 	)
 }
